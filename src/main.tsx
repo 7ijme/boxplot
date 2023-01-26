59b72903dd194ecb0bb1133dedaeb7dedb36dc90
7ijme <68817281+7ijme@users.noreply.github.com>
2023-01-26 23:56:07 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,22 +3,6 @@ import ReactDOM from "react-dom/client";
 import Container from "./Components/Container";
 import "./Styles/index.css";
 
-window.addEventListener("load",function() {
-  // Set a timeout...
-  setTimeout(function(){
-    // Hide the address bar!
-    window.scrollTo(0, 1);
-  }, 0);
-}); 
-
-var preventDefault = function(e: TouchEvent) {
-    e.preventDefault();
-    return false;
-};
-document.addEventListener('touchmove',preventDefault,false);
-document.body.addEventListener('touchmove',preventDefault,true);
-window.addEventListener('touchmove',preventDefault,true);
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Container />
